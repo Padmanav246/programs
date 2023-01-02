c1ee5c04554d15221df041e3413c60ba918ea74f
Srikanth K <padmanav.t@techouts.co.in>
2023-01-02 16:05:08 +0530

    Commited

--- EnumRegex/Regex1.java
+++ EnumRegex/Regex1.java
@@ -2,12 +2,14 @@ package EnumRegex;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import java.util.Scanner;
 public class Regex1 {
 	public static void main(String...strings)
 	{
+		Scanner sc=new Scanner(System.in);
 		String REGEX=".*www.*";
-		String actualstring="www.myweb.com";
+		System.out.println("Enter The web address");
+		String actualstring=sc.nextLine();
 		Pattern pt=Pattern.compile(REGEX);
 		Matcher match=pt.matcher(actualstring);
 		boolean matches=match.matches();
